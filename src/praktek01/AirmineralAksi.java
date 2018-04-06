c71fb59e8cd08f1e2b18cd4193e408d8b12af6c1
Sri Patmawati <sriwatiaz@gmail.com>
2018-04-06 08:34:10 +0800

    Mengisi Nilai Atribut

--- src/praktek01/AirmineralAksi.java
+++ src/praktek01/AirmineralAksi.java
@@ -13,5 +13,27 @@ public class AirmineralAksi {
     public static void main(String[] args) {
         
         Airmineral Una = new Airmineral();
+        
+        Una.warna="Warna Putih";
+        Una.merk="Merk Prof";
+        Una.tanggalexp="TanggalExp 26062018";
+        Una.ukuranair="UkuranAir 600ml";
+        Una.ukuranbotol="UkuranBotol Sedang";
+        
+        Airmineral Nurul = new Airmineral();
+        
+        Nurul.warna="Warna Putih";
+        Nurul.merk="Merk Le Minerale";
+        Nurul.tanggalexp="TanggalExp 27072018";
+        Nurul.ukuranair="UkuranAir 330ml";
+        Nurul.ukuranbotol="UkuranBotol Kecil";
+        
+        Airmineral Zuzun = new Airmineral();
+        
+        Zuzun.warna="Warna Putih";
+        Zuzun.merk="Merk Aqua";
+        Zuzun.tanggalexp="TanggalExp 28082018";
+        Zuzun.ukuranair="UkuranAir 1500ml";
+        Zuzun.ukuranbotol="UkuranBotol Besar";
     }
 }
