64da317e14599f70aca3e3d36330b1236389a2a2
Sri Patmawati <sriwatiaz@gmail.com>
2018-04-06 08:22:08 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Airmineral.java
+++ src/praktek01/Airmineral.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author USER
  */
 public class Airmineral {
-    
+   String warna;
+   String merk;
+   String tanggalexp;
+   String ukuranair;
+   String ukuranbotol;
+   
+   void cetakInfo(){
+       System.out.println("Warna :"+warna);
+       System.out.println("Merk :"+merk);
+       System.out.println("TanggalExp :"+tanggalexp);
+       System.out.println("UkuranAir :"+ukuranair);
+       System.out.println("UkuranBotol :"+ukuranbotol);
+   }
 }
